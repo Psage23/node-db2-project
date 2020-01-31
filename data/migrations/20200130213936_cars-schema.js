9f64b24e2eb7aedabff9d2c055405fcc0282f329
Psage23 <pamelasage3@gmail.com>
2020-01-30 23:22:44 -0700

    seed data is working

--- data/migrations/20200130213936_cars-schema.js
+++ data/migrations/20200130213936_cars-schema.js
@@ -3,11 +3,11 @@ exports.up = function(knex) {
   return knex.schema.createTable('cars', tbl => {
       tbl.increments('carId');
       tbl.decimal('VIN').notNullable();
-      tbl.text('make', 128).unique().notNullable();
-      tbl.text('model', 128).unique().notNullable();
+      tbl.text('make', 128).notNullable();
+      tbl.text('model', 128).notNullable();
       tbl.decimal('mileage').notNullable();
-      tbl.text('engin type', 128);
-      tbl.text('status', 128).unique();
+      tbl.text('engine type', 128);
+      tbl.text('status', 128);
   })
 };
 
